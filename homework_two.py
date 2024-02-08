b9841afe8cca2fc4ce95790068fac4f7b1023b5c
elizabethg60 <eg3055@columbia.edu>
2024-02-08 17:07:33 -0500

    edits to sample lm code

---
+++ homework_two.py
@@ -0,0 +1,14 @@
+"""
+Write a least-squares fitting code that implements the Levenberg-
+Marquardt algorithm
+"""
+"""
+Use the above least-squares fitting code to fit the data hw2 fitting.dat
+on CANVAS with two functions: a Lorentzian as in Equation (1), and a
+Gaussian as in Equation (2)
+"""
+
+"""
+Plot the data and the fitting curves, be sure to include the
+error-bars of the data. Which function fits the data better and why?
+"""
